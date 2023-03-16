64bc3942e8ee73592cdb2a0df2e18dbc86863642
mefdev <mohamedmeftah2018@gmail.com>
2023-03-16 11:11:58 -0700

    Print (Programming is like building a multilingual puzzle) followed by nl

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-/*  Return exatly what inside puts function */
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
