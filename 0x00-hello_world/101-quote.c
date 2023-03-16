68746672df7ecff6f8deabdff23a4a5bfe301857
mefdev <mohamedmeftah2018@gmail.com>
2023-03-16 14:37:44 -0700

    Print what inside putchar function using for loop

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char str_p[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char str_p[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19n";
 int length = sizeof(str_p);
 int i = 0;
 while (i < length)
