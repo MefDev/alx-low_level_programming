2274acbcf4e05be930cc7171bfd5d818cfa25542
mefdev <mohamedmeftah2018@gmail.com>
2023-03-17 16:47:52 -0700

    Print single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print single digit of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;;
+
+	for (digit = 0; digit < 10; digit = digit + 1)
+	{
+		printf("%d", digit);
+	}
+	putchar('\n');
+	return (0);
+}
