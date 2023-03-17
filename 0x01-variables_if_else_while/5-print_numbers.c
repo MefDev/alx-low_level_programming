a890e29339493c203a541809bf458d23add1ab1a
mefdev <mohamedmeftah2018@gmail.com>
2023-03-17 16:49:41 -0700

    Print single digit numbers of base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: print single digit of base 10
+ * Description: Print single digit of base 10
  * Return: 0
  */
 int main(void)
 {
-	int digit;;
+	int digit;
 
 	for (digit = 0; digit < 10; digit = digit + 1)
 	{
