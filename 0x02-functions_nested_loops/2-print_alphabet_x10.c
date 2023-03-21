d9db575f3e2980eebc611a725d443d356a26d601
mefdev <mohamedmeftah2018@gmail.com>
2023-03-21 14:18:52 -0700

    Print the alphabets 10x using putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet 10 times, in lowercase,
+ * followed by a new line
+ * Return: Always 0
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char first_letter = 'a';
+
+		while (first_letter <= 'z')
+		{
+			putchar(first_letter);
+			first_letter++;
+		}
+		putchar('\n');
+	}
+}
