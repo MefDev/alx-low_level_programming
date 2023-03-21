a079199ac37f53cc435685f83efbd44dcbbbbb02
mefdev <mohamedmeftah2018@gmail.com>
2023-03-21 12:55:40 -0700

    Print the alphabet followed by nl

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: prints the alphabet, in lowercase, followed by a nl
+ * Return: 1
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+
+}
+void print_alphabet(void)
+{
+	char first_letter = 'a';
+
+	while (first_letter <= 'z')
+	{
+		putchar(first_letter);
+		first_letter++;
+	}
+	putchar('\n');
+}
