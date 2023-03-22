927f5d28fc70f51da8873f89c855bdaa50af178f
mefdev <mohamedmeftah2018@gmail.com>
2023-03-22 12:02:17 -0700

    Print the 9 times table starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,8 +11,11 @@ void times_table(void)
 	for (rows = 0; rows < 10; rows++)
 	{
 		putchar('0');
-		if (rows != 9)
+		if (rows <= 9)
+		{
 			putchar(',');
+			putchar(' ');
+		}
 		for (columns = 1; columns < 10; columns++)
 		{	printf("%i", columns * rows);
 			if (columns != 9)
