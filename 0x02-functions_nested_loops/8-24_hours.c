e76b1b5f47d5bb40f30609f76a7b94bacb120dcc
mefdev <mohamedmeftah2018@gmail.com>
2023-03-21 18:16:15 -0700

    Print minutes and hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - print the last digit
+ * Return: Always 0
+*/
+int jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	char colon_smbl = ':';
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(colon_smbl);
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			putchar('\n');
+		}
+	}
+	return (0);
+}
