61b83983006a2df96ad4d112205ec1f0f5eac219
mefdev <mohamedmeftah2018@gmail.com>
2023-03-22 17:35:37 -0700

    Print natural numbers less than 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,37 +1,25 @@
 #include <stdio.h>
 /**
- * Discribtion - Print all natural less than 1024
+ * main - Print all natural less than 1024
  * followed by a new line
  * Return: Always 0
 */
 int main(void)
 {
 	int sum;
-	int i;
 	int three;
 	int five;
-	int mutliple_three;
-	int multiple_five;
 
 	sum = 0;
 	three = 3;
 	five = 5;
-	mutliple_three = 0;
-	multiple_five = 0;
-	for (i = 1; i < 1024; i++)
-	{
-		mutliple_three = three * i;
-		if (mutliple_three < 1024)
-		{
-			sum += mutliple_three;
-		}
 
-		multiple_five = five * i;
-		if (multiple_five < 1024)
+	for (int i = 1; i < 1024; i++)
+	{
+		if (i % three == 0 || i % five == 0)
 		{
-			sum += multiple_five;
+			sum += i;
 		}
 	}
 	printf("%i\n", sum);
-	return (0);
 }
