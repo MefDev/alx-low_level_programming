8f7d5f32edf21a35eb44e40b9fbc4a38b4fe4dd1
mefdev <mohamedmeftah2018@gmail.com>
2023-03-23 16:47:35 -0700

    Print from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+	return;
+}
