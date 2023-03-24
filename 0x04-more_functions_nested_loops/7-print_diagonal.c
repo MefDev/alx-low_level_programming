7a37fdb050a8a6fdebf46b92280b69368f82875e
mefdev <mohamedmeftah2018@gmail.com>
2023-03-23 19:11:18 -0700

    Print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - Print diagonal n times
+ * @n: The number of times
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i <  n; i++)
+	{
+		_putchar('/');
+		if (i != n - 1)
+		{
+			_putchar('\n');
+			add_space(i);
+		}
+	}
+	_putchar('\n');
+}
+/**
+ * add_space - add space  n times
+ * @n: The number of times
+ * Return: nothing
+ */
+void add_space(int n)
+{
+	int i;
+
+	_putchar(' ');
+	for (i = 0; i < n; i++)
+	{
+		_putchar(' ');
+	}
+}
