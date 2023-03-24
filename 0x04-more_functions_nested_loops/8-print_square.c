fcc26395e5452bf36637b2e6fce9a2c01ff632bb
mefdev <mohamedmeftah2018@gmail.com>
2023-03-24 08:42:39 -0700

    Print line n times

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,17 @@ void print_square(int size)
 	int i;
 	int row;
 
-	for (row = 0; row < size; row++)
+	if (size > 0)
 	{
-		for (i = 0; i <  size; i++)
+		for (row = 0; row < size; row++)
 		{
-			_putchar('#');
+			for (i = 0; i <  size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
