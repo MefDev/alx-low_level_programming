5b348f2f46a87d70157e6cea1ce4fcb00b4acdff
mefdev <mohamedmeftah2018@gmail.com>
2023-03-27 16:53:39 -0700

    Print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,14 @@
 
 /**
  * print_rev - print the whole string in reverse
- * @str: the string print
+ * @s: the string print
  * Return: void
  */
 
 void print_rev(char *s)
 {
 	int len;
-	int iterate;	
+	int iterate;
 	int i;
 
 	len = 0, iterate = 0;
