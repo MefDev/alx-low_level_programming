9767f52fed73e6767ca26ff221c6310899450dc9
mefdev <mohamedmeftah2018@gmail.com>
2023-03-27 19:51:43 -0700

    Print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		prinf("%d", a[i])
+		printf("%d", a[i]);
 		if (i != n - 1)
 		{
-			prinf(", ");
+			printf(", ");
 		}
 	}
+	printf("\n");
 }
