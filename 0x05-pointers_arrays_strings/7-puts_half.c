a0e14fdc67138e4c93df8147bbed7771b9955d32
mefdev <mohamedmeftah2018@gmail.com>
2023-03-27 19:16:07 -0700

    Print the halvs of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - print halfs
+ * @str: str to print on
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len;
+	int i;
+	int half;
+
+	i = 0, len = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+		len++;
+	}
+	half = len / 2;
+	while (str[half] != '\0')
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	 _putchar('\n');
+}
