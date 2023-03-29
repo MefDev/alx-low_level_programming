600869d34b808ae2479eff0659f014a8864ae47e
mefdev <mohamedmeftah2018@gmail.com>
2023-03-28 19:35:49 -0700

    Implement atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,14 +8,46 @@
 
 int _atoi(char *s)
 {
-	int i;
-	unsigned int n;
+	unsigned int n = 0;
+	int i = 0;
+	int check;
+	int counter = 0;
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		n = n * 10 + (s[i] - 48);
-		i++;
+		if ((s[i] > 47 && s[i] < 58))
+		{
+			n = n * 10 + (s[i] - 48);
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
+		else if (s[i] == '-')
+		{
+			counter++;
+		}
 	}
-	return (n);
+	check = is_negative(counter);
+	return (n * check);
+}
+
+
+/**
+ * is_negative - Check if negative
+ * @counter: The counter
+ * Return: -1 if negative 1 otherwise
+ */
+
+int is_negative(int counter)
+{
+	int negative;
+
+	if (counter % 2 == 1)
+	{
+		negative = -1;
+	}
+	else
+	{
+		negative = 1;
+	}
+	return (negative);
 }
