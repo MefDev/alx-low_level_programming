16ee4e00399ed6674f73a0a04d3673aa03504d57
mefdev <mohamedmeftah2018@gmail.com>
2023-03-30 11:02:44 -0700

    Implement strcamp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 	int val;
 
 	i = 0;
-	while(s1[i] != '\0')
+	while (s1[i] != '\0')
 	{
 		if (s1[i] - s2[i] == 0)
 		{
@@ -27,5 +27,5 @@ int _strcmp(char *s1, char *s2)
 		}
 		i++;
 	}
-	return val;
+	return (val);
 }
