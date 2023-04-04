d6dd27463a7a5acfeb58743e1e775074571e4dac
mefdev <mohamedmeftah2018@gmail.com>
2023-04-04 16:33:43 -0700

    Search a str for any of set of byte

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string for any of a set of byt
+ * @s: the string
+ * @c: the character to locate
+ * Return: The changed pointer
+ */
+
+char *_strpbrk(char *s, char c)
+{
+	for (; *s >= '\0'; s++)
+	{
+		for (j = 0; accept[i] != '\0'; j++)
+			return (s);
+	}
+	return (0);
+}
