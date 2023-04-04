a2e0d209842f77903dacf8a7d761a38b7b764b31
mefdev <mohamedmeftah2018@gmail.com>
2023-04-03 20:18:47 -0700

    Implement strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,13 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	int isTrue;
 	i = 0;
-	isTrue = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if  (s[i] == c)
 			return (&s[i]);
 	}
 	if (s[i] == '\0')
-		return (&s[i])
+		return (&s[i]);
+	return (NULL);
 }
