b2b37374b0c2e838e9ef0ce97abb17e379cc70d9
mefdev <mohamedmeftah2018@gmail.com>
2023-04-03 19:57:04 -0700

    test

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,15 @@ char *_strchr(char *s, char c)
 {
 	int i;
 	int isTrue;
-
+	i = 0;
 	isTrue = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if  (s[i] == c)
 			return (&s[i]);
 	}
-
-	return (0);
+	if (s[i] == '\0')
+		return (s + i)
+	return (NULL);
 
 }
