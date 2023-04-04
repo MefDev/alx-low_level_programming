a16221d3dd7092c6223230059ec82ea27788da94
mefdev <mohamedmeftah2018@gmail.com>
2023-04-04 15:58:34 -0700

    Locate char in str

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,10 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	i = 0;
-	for (i = 0; s[i] >= \0'; i++)
+	for (; *s >= \0'; s++)
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		if (*s == c)
+			return (s);
 	}
 	return (0);
 }
