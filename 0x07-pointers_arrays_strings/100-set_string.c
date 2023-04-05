bd8f80807be17356d3dfb442439dc208a7c35528
mefdev <mohamedmeftah2018@gmail.com>
2023-04-04 19:48:42 -0700

    Pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - print the chessboard
+ * @s: pointer to pointer
+ * @to: pointer
+ * Return: none
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+
+}
