04ebc370d194fb889817bbf00c25c9af81b67372
mefdev <mohamedmeftah2018@gmail.com>
2023-04-10 16:33:23 -0700

    TEST

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
 #include <stddef.h>
-
+#ifndef MAIN_H
+#define MAIN_H
 int count_str(char *str);
 int is_negative(int counter);
 int _putchar(char c);
@@ -22,3 +23,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif /* MAIN_H */
