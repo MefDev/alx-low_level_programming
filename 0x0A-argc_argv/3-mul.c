0dbbf80a4a59abeee051a2d2354c45eaacaa94b5
mefdev <mohamedmeftah2018@gmail.com>
2023-04-10 19:50:56 -0700

    Multiply two args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print the two args followed by nl
+ * @argc: count how many args on argv array
+ * @argv: the argments given by the user on the CLI
+ * Return: success 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum =  atoi(argv[1]) * atoi(argv[2]);
+	printf("%d", sum);
+	return (0);
+}
