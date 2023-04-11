5224f27664061572e06181dabe6a6e36fb7cd3b9
mefdev <mohamedmeftah2018@gmail.com>
2023-04-11 10:46:57 -0700

    Add two args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,42 @@
 #include "main.h"
 
 /**
+ * isNumber - check if the char is a digit
+ * @num: array of chars
+ * Return: success 0 and 1 failure
+ */
+int isNumber(char num[])
+{
+	int i;
+
+	i = 0;
+	if (num[0] == '-')
+		i = 1;
+	for (; num[i] != 0; i++)
+	{
+		if (!isdigit(num[i]))
+			return (0);
+	}
+	return (1);
+}
+
+/**
  * main - print the two args followed by nl
  * @argc: count how many args on argv array
  * @argv: the argments given by the user on the CLI
  * Return: success 0 and 1 failure
  */
+
 int main(int argc, char *argv[])
 {
 	int i, sum;
 
 	sum = 0;
-	if (argc > 2)
+	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (!atoi(argv[i]))
+			if (!isNumber(argv[i]))
 			{
 				printf("Error\n");
 				return (1);
