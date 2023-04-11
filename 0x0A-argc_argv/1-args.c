7acde528f0d5bb9eb3dc021226a29329d5ea5d88
mefdev <mohamedmeftah2018@gmail.com>
2023-04-10 18:27:53 -0700

    Check how many args the user passes

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print the number of args followed by nl
+ * @argc: count how many args on argv array
+ * @argv: the argments given by the user on the CLI
+ * Return: argments name if args written by the user 'no args found otherwise'
+ */
+int main(int argc, char *argv[])
+{
+	return (argv[0] !=  0 ? printf("%d\n", argc - 1) :
+		printf("%s", "No args found"));
+}
