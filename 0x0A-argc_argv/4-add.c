2361fc422a32a2cd68c9873c78cbf26b53f8ff3f
mefdev <mohamedmeftah2018@gmail.com>
2023-04-11 09:48:57 -0700

    Add two args

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,20 +10,21 @@ int main(int argc, char *argv[])
 {
 	int i, sum;
 
-	if (argc == 1)
+	sum = 0;
+	if (argc > 1)
 	{
-		printf("0\n");
-		return (1);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		if (!atoi(argv[i]))
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
 		}
-		sum += atoi(argv[i]);
+		printf("%d\n", sum);
+		return (0);
 	}
-	printf("%d\n", sum);
-	return (0);
+	printf("%d\n", 0);
+	return (1);
 }
