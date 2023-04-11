db558e9396143ade6e1d44f353ee71f2809a55d0
mefdev <mohamedmeftah2018@gmail.com>
2023-04-11 11:24:38 -0700

    Print mininum number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	int i, rest, cents;
 	int values[] = {25, 10, 5, 2, 1};
 
-	if (argc != 2)
+	if (argc != 2 || !isNumber(argv[1]))
 	{
 		printf("Error\n");
 		return (1);
@@ -42,3 +42,24 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
+
+/**
+ * isNumber - check if the char is a digit
+ * @num: array of chars
+ * Return: success 0 and 1 failure
+ */
+int isNumber(char num[])
+{
+	int i;
+
+	i = 0;
+	if (num[0] == '-')
+		i = 1;
+	for (; num[i] != 0; i++)
+	{
+		if (!isdigit(num[i]))
+			return (0);
+	}
+	return (1);
+}
+
