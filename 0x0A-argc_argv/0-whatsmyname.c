936272c5d538864347716334513b6a79bbef05a9
mefdev <mohamedmeftah2018@gmail.com>
2023-04-10 18:19:46 -0700

    Print args name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - print the name of args followed by nl
  * @argc: count how many args on argv array
  * @argv: the argments given by the user on the CLI
- * Return: argments' name if args written by the user 'no args found otherwise'
+ * Return: argments name if args written by the user 'no args found otherwise'
  */
 int main(int argc, char *argv[])
 {
