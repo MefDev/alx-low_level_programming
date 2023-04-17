ce27303b9c83efa1c938ae979e4457f9d088f18a
mefdev <mohamedmeftah2018@gmail.com>
2023-04-17 13:33:20 -0700

    Add prototypes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+
+/**
+ * struct dog - Short desciption
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
