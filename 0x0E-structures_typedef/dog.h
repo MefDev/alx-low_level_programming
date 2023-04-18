99fca26888ded8d46d74f07d70e76f96ccd145e0
mefdev <mohamedmeftah2018@gmail.com>
2023-04-17 18:21:23 -0700

    Print dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,8 +16,9 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+} dog;
 #endif
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
