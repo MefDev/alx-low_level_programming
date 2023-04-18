8766ac60f2eab0a53fbb574bed4af324b39368d2
mefdev <mohamedmeftah2018@gmail.com>
2023-04-17 18:09:29 -0700

    Add Proto

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #ifndef DOG
 #define DOG
 /**
@@ -15,6 +16,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 #endif
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
