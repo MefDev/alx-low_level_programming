818408ebad12ca07f7cda00744b62cc8b4e036a2
mefdev <mohamedmeftah2018@gmail.com>
2023-04-20 19:46:42 -0700

    Add prototypes

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,11 +8,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum, i;
+	unsigned int i;
+	int sum;
 	va_list args;
 
 	if (n == 0)
 		return (0);
+
 	va_start(args, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
