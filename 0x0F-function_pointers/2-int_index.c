763f0fc8d8adac206c01c98ad6a041c693de8169
mefdev <mohamedmeftah2018@gmail.com>
2023-04-21 16:37:04 -0700

    Seach fro an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,12 +1,32 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Print a name
- * @name: the name to print
- * @f: a function to callback
+ * int_index - seach for an integer
+ * @array: arr of elem
+ * @size: the length of the arr
+ * @cmp: compare function pointer
+ * Return: the index or - 1 if not found
  */
 
-void print_name(char *name, void (*f)(char *))
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	
+	if (array && cmp)
+	{
+		int i, found;
+
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			found = -1;
+			if (cmp(array[i]) != 0)
+			{
+				found = i;
+				return (found);
+			}
+		}
+		return (found);
+	}
+	else
+		return (-1);
 }
