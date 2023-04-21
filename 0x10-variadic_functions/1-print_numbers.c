bba26d244c9c283bac2109f2d6b5f989e835f872
mefdev <mohamedmeftah2018@gmail.com>
2023-04-20 20:07:25 -0700

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,20 +7,19 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    int i, arg;
-    va_list args;
-    va_start(args, n);
-    for (i = 0; i < n; i++)
-    {
-        arg = va_arg(args, int);
-        printf("%d", arg);
-        if (i != n - 1 && separator != NULL)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-       
-    }
-    printf("\n");
-    va_end(args);
+	unsigned int i, arg;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		arg = va_arg(args, int);
+		printf("%d", arg);
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
 }
