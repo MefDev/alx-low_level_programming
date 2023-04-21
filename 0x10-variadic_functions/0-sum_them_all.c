d3e730cd2834ae3446d9d20bd3fa19e5954ffff4
mefdev <mohamedmeftah2018@gmail.com>
2023-04-20 19:35:18 -0700

    Print the sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum of all its parameters
+ * @n: count of parameters
+ * Return: sum or 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum, i;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
