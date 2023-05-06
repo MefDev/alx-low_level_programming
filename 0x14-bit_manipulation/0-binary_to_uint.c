9cbe7ed792c8e9c4624f8bae1e9d435cc1906a0b
mefdev <mohamedmeftah2018@gmail.com>
2023-05-05 17:48:51 -0700

    Convert to uint binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		convert_num =  (convert_num << 1) + (b[i] - 48);
+		convert_num =  (convert_num << 1) + (b[i] - '0');
 	}
 	return (convert_num);
 }
