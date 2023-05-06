8c1229adf67133f5a7453a0487103985cb66ea0d
mefdev <mohamedmeftah2018@gmail.com>
2023-05-05 18:23:45 -0700

    Add protoytpes

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stddef.h>
+#include <stdio.h>
 unsigned int binary_to_uint(const char *);
 void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
