6ddb6e42d589f1b3b203066e78a5961f3422c6b6
mefdev <mohamedmeftah2018@gmail.com>
2023-05-11 11:01:28 -0700

    copy a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,39 +9,32 @@
 
 int main(int argc, char *argv[])
 {
-	char *file_from;
-	char *file_to;
-	FILE *src;
-	FILE *dest;
+	int file_from, file_to, read_result;
+	char buffer[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
-	}
-	file_from = argv[1];
-	file_to = argv[2];
-
-	src = fopen(file_from, "r");
-	if (!src)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	dest = fopen(file_to, "w");
-	if (!dest)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
-		exit(99);
-	}
-
-	copy_file(src, dest);
-
-	if (fclose(src) == -1 || fclose(dest) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				argv[1]), exit(98);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				argv[2]), exit(99);
+	while ((read_result = read(file_from, buffer, 1024)) > 0)
+		if (write(file_to, buffer, read_result) != read_result)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+					argv[2]), exit(99);
+	if (read_result == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				argv[1]), exit(98);
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+				file_from), exit(100);
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+				file_to), exit(100);
 	return (0);
 }
