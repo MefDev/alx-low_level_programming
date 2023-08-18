b14622a09eb21cfe351c10155e166305d34ecf1c
mefdev <mohamedmeftah2018@gmail.com>
2023-08-18 01:12:53 +0100

    TEST

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -32,9 +32,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (curr == NULL)
 		return (-1);
 
-	current->prev->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = current->prev;
-	free(current);
+	curr->prev->next = curr->next;
+	if (curr->next != NULL)
+		curr->next->prev = curr->prev;
+	free(curr);
 	return (1);
 }
