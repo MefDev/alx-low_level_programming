26ab4c7f1db47a3e68c916fb8cf1166343f00d97
mefdev <mohamedmeftah2018@gmail.com>
2023-08-19 00:27:04 +0100

    Insert at a spicific idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - return the nth node of a LL
- * @head: point to the start of a linked list
- * @index: index of the node
+ * insert_dnodeint_at_index - return the nth node of a LL
+ * @h: double pointer to the start of a linked list
+ * @idx: index of the node
+ * @n: integer
  * Return: the nth node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
@@ -13,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	if (!h)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
@@ -23,12 +24,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = NULL;
 	new_node->next = NULL;
 
-	if (idx == 0) {
+	if (idx == 0)
+	{
 		new_node->next = *h;
 		if (*h)
 			(*h)->prev = new_node;
 		*h = new_node;
-	} else {
+	}
+	else
+	{
 		 current = *h;
 		for (i = 0; i < idx - 1 && current; i++)
 			current = current->next;
