23e7ca1b518e598e37127874b042bca2164944e4
mefdev <mohamedmeftah2018@gmail.com>
2023-08-18 01:12:19 +0100

    TEST

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index- delete the node at index
+ * index of a dlistint_t linked list
+ * @head: start of doubly linked list
+ * @index: index to insert val
+ * Return: return 1 if succesful -1 if otherwise
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *curr = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = curr->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(curr);
+		return (1);
+	}
+
+	for (i = 0; curr != NULL && i < index; i++)
+		curr = curr->next;
+
+	if (curr == NULL)
+		return (-1);
+
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+	return (1);
+}
