780f84bf596060d4c26f4c45665d646ecdde9b24
mefdev <mohamedmeftah2018@gmail.com>
2023-10-26 01:43:05 +0100

    Interview question answered

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+    A function to calculate the permiter of a grid
+"""
+
 
 def island_perimeter(grid):
+    """Calculate the perimeter of a grid"""
     rows = len(grid)
     columns = len(grid[0])
     perimeter = 0
@@ -10,17 +15,12 @@ def island_perimeter(grid):
                 land_cell = grid[row][column]
                 if land_cell == 1:
                     if row == 0 or grid[row-1][column] == 0:
-                            perimeter += 1
+                        perimeter += 1
                     if row == rows - 1 or grid[row+1][column] == 0:
                         perimeter += 1
                     if column == 0 or grid[row][column-1] == 0:
                         perimeter += 1
                     if column == columns - 1 or grid[row][column+1] == 0:
                         perimeter += 1
-    
+
     return perimeter
-
-
-
-
-    
